ab354dad93da12779341d3e04c4ce86f6f5d18d3
Carina Grode <106753609+carinagrode@users.noreply.github.com>
2023-10-22 10:52:12 +0200

    Comment out cursor and input functions

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
+// Next steps:
+// - Delete browser default input field: blue border
+// - Add cursor
+// - Make lines fade out
+
+
+
 const textArea = document.getElementById('textArea');
 
 textArea.focus();
 
-function createCursor() {
-    const cursor = document.createElement('span');
-    cursor.className = 'cursor';
-    cursor.textContent = '|';
-    return cursor;
-}
+// function createCursor() {
+//     const cursor = document.createElement('span');
+//     cursor.className = 'cursor';
+//     cursor.textContent = '|';
+//     return cursor;
+// }
 
-textArea.appendChild(createCursor());
+// textArea.appendChild(createCursor());
+
+textArea.addEventListener('input', displayInputText);
+
+function displayInputText() {
+    // const cursor = textArea.querySelector('.cursor');
+    // const text = textArea.textContent;
+    // textArea.innerHTML = text;
+    // textArea.appendChild(cursor);
+}
